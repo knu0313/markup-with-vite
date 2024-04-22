bddc85d961d2aa9ac93d3aa867f816c5b478cb3e
Namuk Kang <knu0313@gmail.com>
2024-04-22 21:33:15 +0900

    first set

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default {
+  plugins: [
+    handlebars({
+      context: {
+        title: 'Hello, world!',
+      },
+      partialDirectory: resolve(__dirname, 'src/_partials'),
+    }),
+  ],
+};
