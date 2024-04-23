c23b529a66d953fbbd07f12b212f3b457f6148a2
knu <knu0313@gmail.com>
2024-04-23 05:32:52 +0000

    수정

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,45 @@
-import { resolve } from 'path';
 import copy from 'rollup-plugin-copy';
 import handlebars from 'vite-plugin-handlebars';
 import autoprefixer from 'autoprefixer';
+import fs from 'fs';
+import path, { resolve } from 'path';
+
+// 특정 폴더내에 html 파일 input entries 만들기
+const getHtmlEntries = dir => {
+  const htmlEntries = {};
+  fs.readdirSync(dir).forEach(item => {
+    const itemPath = path.join(dir, item);
+
+    if(fs.statSync(itemPath).isFile()) {
+      if(path.extname(item) == '.html') {
+        htmlEntries[item] = resolve(__dirname, itemPath);
+      }
+    } else {
+      Object.assign(htmlEntries, getHtmlEntries(itemPath));
+    }
+  });
+
+  return htmlEntries;
+};
 
 export default {
   root: 'src',
   assetsDir: 'src',
   build: {
     outDir: '../dist/',
-    // emptyOutDir: true,
+    emptyOutDir: true,
     rollupOptions: {
+      input: getHtmlEntries('src'),
       plugins: [
+        handlebars({
+          partialDirectory: resolve(__dirname, 'src/_partials'), //partials 경로 설정
+        }),
         copy({
           targets: [
             { src: 'src/img', dest: 'dist/' } // 에셋 파일을 복사할 소스 및 대상 경로
           ],
           verbose: true // 복사 작업을 로그에 표시할지 여부
-        })
+        }),
       ]
     }
   },
@@ -29,9 +52,6 @@ export default {
   },
   plugins: [
     handlebars({
-      context: {
-        title: 'Hello, world!',
-      },
       partialDirectory: resolve(__dirname, 'src/_partials'), //partials 경로 설정
     }),
   ],
